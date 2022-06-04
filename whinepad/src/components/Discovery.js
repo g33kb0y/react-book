afdaea6ec4faf395a16d8b4f86286d9a6f825558
John Jacobsen <john.jacobsen@gmail.com>
2022-06-04 13:50:21 -0400

    finishging chapter 7 - warnings

--- whinepad/src/components/Discovery.js
+++ whinepad/src/components/Discovery.js
@@ -5,14 +5,78 @@ import Button from './Button';
 import Suggest from './Suggest';
 import Rating from './Rating';
 import FormInput from './FormInput';
+import Form from './Form';
+import { useRef, useState } from 'react';
+import Actions from './Actions';
+import Dialog from './Dialog';
+import schema from '../config/schema';
 
 function Discovery() {
+    const form = useRef();
+
+    function DialogExample() {
+        const [example, setExample] = useState(null);
+        return (
+            <>
+                <p>
+                    <Button onClick={() => setExample(1)}>Example 1</Button>{' '}
+                    <Button onClick={() => setExample(2)}>Example 2</Button>
+                </p>
+                {example === 1 ? (
+                    <Dialog modal
+                            header="Out of the box example"
+                            onAction={(type) => {
+                                alert(type);
+                                setExample(null);
+                            }}>Hello, dialog!
+                    </Dialog>
+                ) : null}
+                {example === 2 ? (
+                    <Dialog header="Not Modal, custom dismiss"
+                            hasCancel={false}
+                            confirmLabel="Whatever"
+                            onAction={(type) => {
+                                alert(type);
+                                setExample(null);
+                            }}>
+                        Anything goes in here, like a <Button>button</Button> for example
+                    </Dialog>
+                ) : null}
+            </>
+        );
+    }
     return (
         <div className="Discovery">
             <h2>Logo</h2>
-            <div sytle={{backgroun: '#f6f6f6', display: 'inline-block'}}>
+            <div style={{backgroun: '#f6f6f6', display: 'inline-block'}}>
                 <Logo />
             </div>
+
+            <h2>Dialog</h2>
+            <div>
+                {DialogExample()}
+            </div>
+
+            <h2>Form</h2>
+            <div>
+                <Form ref={form}
+                      fields={{
+                          rateme: {label: 'Rating', type: 'rating'},
+                          freetext: {label: 'Greetings'},
+                      }}
+                      initialData={{rateme: 4, freetext: 'Hi'}}
+                />
+                <Button onClick={() => {
+                    const data = {};
+                    Array.from(form.current).forEach(
+                        (input) => (data[input.id] = input.value),
+                    );
+                    alert(JSON.stringify(data));
+                }}>Submit</Button>
+            </div>
+
+            <h2>Actions</h2>
+            <Actions onAction={(type) => alert(type)} />
 
             <h2>Button</h2>
             <p>
@@ -80,12 +144,24 @@ function Discovery() {
             <Body>Content inside of the Body tag</Body>
 
             <h2>Excel</h2>
-            <Excel headers={['Name','Year']}
-                   initialData={[
-                       ['Charles','1859'],
-                       ['Antoine','1943'],
-                   ]}
+            <Excel schema={schema}
+                   initialData={schema.name.samples.map((_,idx) => {
+                       const element = {};
+                       for (let key in schema) {
+                           element[key] = schema[key].samples[idx];
+                       }
+                       return element;
+                   })}
+                   onDataChange={(data) => {
+                       console.log(data);
+                   }}
             />
+            {/*<Excel headers={['Name','Year']}*/}
+            {/*       initialData={[*/}
+            {/*           ['Charles','1859'],*/}
+            {/*           ['Antoine','1943'],*/}
+            {/*       ]}*/}
+            {/*/>*/}
         </div>
     );
 }
---
+++ whinepad/src/config/classification.js
@@ -0,0 +1,11 @@
+const classification = {
+    grapes: [
+        'Baco Noir',
+        'Barbera',
+        'Blueberries',
+        'Cabernet Franc',
+        'Cabernet Sauvignon',
+    ],
+};
+
+export default classification;
